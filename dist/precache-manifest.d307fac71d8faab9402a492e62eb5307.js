bef89b0e403413d07dbc8d533b0f4f1c8dc1779e
jhonatan <jhonatan98rios@gmail.com>
2021-01-28 04:16:33 -0800

    dist deploy

---
+++ dist/precache-manifest.d307fac71d8faab9402a492e62eb5307.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caacd9f7f28a0ce25574",
+    "url": "/css/app.fb939980.css"
+  },
+  {
+    "revision": "b578ee8b4644669a896e4ed2c5da628c",
+    "url": "/img/attack.b578ee8b.svg"
+  },
+  {
+    "revision": "24fa82cee8ad4a13b3285b315849f412",
+    "url": "/img/bg.24fa82ce.jpg"
+  },
+  {
+    "revision": "82329a41efebbf477101bbb882da602b",
+    "url": "/img/defense.82329a41.svg"
+  },
+  {
+    "revision": "55b6720190e30c7c31ce0b3cf3c4b22c",
+    "url": "/img/itens.55b67201.svg"
+  },
+  {
+    "revision": "ab509278025954109fde747b7be15d05",
+    "url": "/img/rsz_attack.ab509278.png"
+  },
+  {
+    "revision": "81441c40b59604d4f844ede78dd4a580",
+    "url": "/img/rsz_default.81441c40.png"
+  },
+  {
+    "revision": "267733678303763317a00a8b344f181f",
+    "url": "/img/rsz_died.26773367.png"
+  },
+  {
+    "revision": "91d619e4a96e0a36d406fd0c0f2be473",
+    "url": "/img/rsz_hurt.91d619e4.png"
+  },
+  {
+    "revision": "891369c2c17786e8c78118d2abd73d52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caacd9f7f28a0ce25574",
+    "url": "/js/app.5cb100fa.js"
+  },
+  {
+    "revision": "196ee371ee68d264db88",
+    "url": "/js/chunk-vendors.6dade5a4.js"
+  },
+  {
+    "revision": "c9b1faeccc4214cca80995e01bd69414",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
