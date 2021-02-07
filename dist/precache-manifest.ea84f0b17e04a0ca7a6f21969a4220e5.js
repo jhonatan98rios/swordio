cde2c46709fc4ae32696c2858b60bf300f9bb6fd
jhonatan <jhonatan98rios@gmail.com>
2021-02-06 19:37:17 -0800

    build

---
+++ dist/precache-manifest.ea84f0b17e04a0ca7a6f21969a4220e5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03b1b92dd8416c4d839",
+    "url": "/css/app.4005dae6.css"
+  },
+  {
+    "revision": "b578ee8b4644669a896e4ed2c5da628c",
+    "url": "/img/attack.b578ee8b.svg"
+  },
+  {
+    "revision": "24fa82cee8ad4a13b3285b315849f412",
+    "url": "/img/bg.24fa82ce.jpg"
+  },
+  {
+    "revision": "82329a41efebbf477101bbb882da602b",
+    "url": "/img/defense.82329a41.svg"
+  },
+  {
+    "revision": "55b6720190e30c7c31ce0b3cf3c4b22c",
+    "url": "/img/itens.55b67201.svg"
+  },
+  {
+    "revision": "4514226cf0b55aa35d2b765e89b007c5",
+    "url": "/img/spritesheet.4514226c.png"
+  },
+  {
+    "revision": "1da9488248fb97cbc3914ccfc6a8cb15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a03b1b92dd8416c4d839",
+    "url": "/js/app.98aeffcc.js"
+  },
+  {
+    "revision": "b1a972a14cfde570abc4",
+    "url": "/js/chunk-vendors.bcfe4c5c.js"
+  },
+  {
+    "revision": "c9b1faeccc4214cca80995e01bd69414",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
